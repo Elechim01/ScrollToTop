dd3c3324e79cc81fb2ceb5e9743f4203feb4ea9e
Elechim01 <mikymanny015@gmail.com>
2021-04-28 19:34:52 +0200

    Initial Commit

---
+++ Shared/ScrollToTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToTopApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
